3116acb3d72668916d3291423160032872c3124d
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2025-11-19 11:06:02 +0000

    feat: initialize project structure with model, training, and testing components

---
+++ src/pytest_ml_tdd_example/training/test.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data
+
+
+def test(
+    model: nn.Module, device: torch.device, test_loader: torch.utils.data.DataLoader
+) -> None:
+    model.eval()
+
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(
+                output, target, reduction="sum"
+            ).item()  # sum up batch loss
+            pred = output.argmax(
+                dim=1, keepdim=True
+            )  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100.0 * correct / len(test_loader.dataset),
+        )
+    )
